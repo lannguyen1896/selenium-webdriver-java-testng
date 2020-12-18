f2220dccca55d6fdd6bd949a5250765818e7db0f
lannguyen1807 <lannguyen1807@DESKTOP-TNP39HQ>
2020-12-18 10:00:09 +0700

    The first commit

---
+++ webdriver/TestNG.java
@@ -0,0 +1,32 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+
+public class TestNG {
+  WebDriver driver;
+  
+  @BeforeClass
+  public void beforeClass() {
+	  //mo trinh duyet
+	  driver = new FirefoxDriver();
+	  driver.get("https://google.com/");
+  }
+  
+  @Test
+  public void TC_01_Login_With_Empty_Email() {
+	  
+  }
+  
+  @Test
+  public void TC_01_Login_With_Invalid_Email() {
+	  
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+
+}
